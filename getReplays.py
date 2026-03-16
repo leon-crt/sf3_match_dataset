ef46813c3a60bd76f82efdba4f50acd53ecfbfc7
leon-crt <leon.pirazzoli00@gmail.com>
2026-03-16 15:41:48 +0100

    script is downloading a file but it cannot be read by fbneo so something is wrong

--- getReplays.py
+++ getReplays.py
@@ -1,33 +1,69 @@
-import requests
-import undetected_chromedriver as uc
-from selenium.webdriver.common.by import By
-fcade_url = "https://www.fightcade.com/api/"
-body = '{"req": "getuser", "username": "gureepufu"}'
-headers = {'content-type' : 'application/json'}
+import socket
+import os
 
-class CustomUCWebDriver(uc.Chrome):
-    def post(self, url, data):
-        return self.execute_script(f"""
-            return fetch("{url}", {{
-                method: "POST",
-                body: '{data}',
-                headers: {{
-                    "Content-Type": "application/json;charset=UTF-8"
-                }}
-            }})
-            .then(response => response.text());
-        """)
+fcade_address = '141.94.138.123'
+fcade_port = 7100
+
+fcade_socket = socket.create_connection(address=(fcade_address, fcade_port))
+
+quark_id_subfix = '9749'
+quark_id = '1773245934968-9749.7'
+# samcog vs sleepw4lker -> 1773245934968-9749.7
+# pngyakuza vs ghusek -> 1773245874617-6933.7
+
+msg_1 = b'\x00\x00\x00\x14'
+
+msg_2 = b'\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x1d\x00\x00\x00\x01'
+
+msg_3 = b'\x00\x00\x00 '
+
+msg_4 = b'\x00\x00\x00\x02\x00\x00\x00\x14\x00\x00\x00\x14' + quark_id.encode() + b'\x00\x00\x00 \x00\x00\x00\x03\x00\x00\x00\x0c\x00\x00\x00\x14' + quark_id.encode()
+
+msg_arr = {msg_1, msg_2, msg_3, msg_4}
 
 
-# Create an instance of CustomUCWebDriver
-options = uc.ChromeOptions()
-options.add_argument("--headless=new")  # for hidden mode
-options.add_argument("--use_subprocess=False") 
+def send_msg(msg:str, socket:socket, rcv:bool, sz=0) -> bytes:
+    socket.send(msg)
+    data = ''
+    if rcv:
+        data = socket.recv(sz)
+    return data
 
-driver = CustomUCWebDriver(options=options)
+class Player_Info:
+    name:str
+    rank:str
+    matches:str
+    country:str
+    def __init__(self, query_str):
+        query_str = query_str.replace('\x00', '')
+        query_str = query_str.replace('\r', '')
+        query_str = query_str.replace('\x12', '')
 
-# Make a POST request and capture the response
-response_data = driver.post(fcade_url, body)
+        name_rank, self.matches, self.country = query_str.split(',')
+        self.name, self.rank = name_rank.split('#')
 
-# Print the response data
-print(response_data)
+# for msg in msg_arr:
+#     data = 'ack'
+#     if msg == msg_2 or msg == msg_4:
+#         data = send_msg(msg, fcade_socket, True)
+#     else:
+#         send_msg(msg, fcade_socket, False)
+#     print(data)
+
+send_msg(msg_1, fcade_socket, False)
+print(send_msg(msg_2, fcade_socket, True, 66))
+send_msg(msg_3, fcade_socket, False)
+
+query_b = send_msg(msg_4, fcade_socket, True, 113)
+query:str = query_b.decode(encoding='utf-8')
+player_1_info = Player_Info(query[16:32])
+player_2_info = Player_Info(query[32:51])
+
+file_path = player_1_info.name + '_' + player_2_info.name + '_' + quark_id_subfix + '.fr'
+with open(file_path, 'wb+') as replay_f:
+    buf = fcade_socket.recv(1024)
+    while len(buf) > 0:
+        buf = fcade_socket.recv(1024)
+        replay_f.write(buf)
+
+fcade_socket.close()
